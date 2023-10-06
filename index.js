54fd639221755ceaff112fd4eb1011e529da25fa
Fahim Hossain <67559866+sk-fahim-hossain@users.noreply.github.com>
2023-10-07 01:49:00 +0600

    comment added

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ app.get('/places/:id',(req,res) => {
     res.send(destination)
     console.log(`destination ${destination}`)
 })
-
+// all datas of diffrent hotels
 app.get('/hotel',(req,res) => {
     res.send(hotelData)
 })
+
+// specific hotel  data 
 app.get('/hotel/:id',(req,res) => {
     const hotelId = req.params.id
     const hotelName = hotelData.find(hotel => hotel.id ==hotelId)
